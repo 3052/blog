1c3492d3905ec74ba5441d641255f1eca10f8c99
Steven Penny <spen+3052@protonmail.com>
2023-10-09 21:35:43 -0500

    new file:   2023-10/youtube-views/youtube-views.go

--- 2023-10/go.mod
+++ 2023-10/go.mod
@@ -2,6 +2,17 @@ module blog
 
 go 1.21.0
 
-require 154.pages.dev/protobuf v1.0.2
+require (
+	154.pages.dev/media v1.6.6
+	154.pages.dev/protobuf v1.0.2
+)
 
-require google.golang.org/protobuf v1.31.0 // indirect
+require (
+	154.pages.dev/encoding v1.12.7 // indirect
+	154.pages.dev/http v1.1.5 // indirect
+	154.pages.dev/strconv v1.0.5 // indirect
+	154.pages.dev/widevine v1.1.4 // indirect
+	github.com/Eyevinn/mp4ff v0.38.1 // indirect
+	github.com/chmike/cmac-go v1.1.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ 2023-10/youtube-views/youtube-views.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+   "154.pages.dev/media/youtube"
+   "fmt"
+   "os"
+   "strconv"
+   "time"
+)
+
+func views_per_year(play *youtube.Player) (string, error) {
+   views, err := func() (float64, error) {
+      t, err := play.Time()
+      if err != nil {
+         return 0, err
+      }
+      y := time.Since(t).Hours() / 24 / 365
+      return float64(play.Video_Details.View_Count) / y, nil
+   }()
+   if err != nil {
+      return "", err
+   }
+   var b []byte
+   if views >= 10_000_000 {
+      b = append(b, red...)
+      b = append(b, " FAIL "...)
+   } else {
+      b = append(b, green...)
+      b = append(b, " PASS "...)
+   }
+   b = append(b, reset...)
+   b = append(b, "   "...)
+   b = strconv.AppendFloat(b, views, 'f', 0, 64)
+   b = append(b, "   "...)
+   b = append(b, play.Video_Details.Video_ID...)
+   return string(b), nil
+}
+
+func main() {
+   if len(os.Args) == 2 {
+      var req youtube.Request
+      req.Video_ID = os.Args[1]
+      req.Mobile_Web()
+      play, err := req.Player(nil)
+      if err != nil {
+         panic(err)
+      }
+      views, err := views_per_year(play)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Println(views)
+   } else {
+      fmt.Println("youtube-views [video ID]")
+   }
+}
+
+const (
+   reset = "\x1b[m"
+   green = "\x1b[30;102m"
+   red = "\x1b[30;101m"
+)
