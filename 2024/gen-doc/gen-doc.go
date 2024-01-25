349192ff8d0c543e36cf012d271c25487a798c96
3052 <spen+3052@protonmail.com>
2024-01-25 06:00:47 -0600

    +   checkout := flag.Bool("c", false, "checkout")

--- 2024/gen-doc/gen-doc.go
+++ 2024/gen-doc/gen-doc.go
@@ -15,37 +15,41 @@ func run(name string, arg ...string) error {
 }
 
 func main() {
+   checkout := flag.Bool("c", false, "checkout")
+   module := flag.String("m", "", "module")
    version := flag.String("v", "", "version")
-   module := flag.String("m", "", `module (default "std")`)
    flag.Parse()
    if *version != "" {
-      err := run("git", "checkout", *version)
-      if err != nil {
-         panic(err)
-      }
       temp, err := os.MkdirTemp(".", "")
       if err != nil {
          panic(err)
       }
-      arg := []string{"-out", temp, "-pkg-version", *version}
-      if *module != "" {
-         arg = append(arg, "-home", *module, "./...")
-      } else {
-         arg = append(arg, "std")
+      args := []string{
+         "-home", *module,
+         "-out", temp,
+         "-pkg-version", *version,
+         "./...",
       }
-      if err := run("doc2go", arg...); err != nil {
+      if *checkout {
+         if err := run("git", "checkout", *version); err != nil {
+            panic(err)
+         }
+      }
+      if err := run("doc2go", args...); err != nil {
          panic(err)
       }
-      if err := os.WriteFile(temp+"/.nojekyll", nil, 0666); err != nil {
-         panic(err)
-      }
-      if err := run("git", "checkout", "main"); err != nil {
-         panic(err)
+      if *checkout {
+         if err := run("git", "checkout", "main"); err != nil {
+            panic(err)
+         }
       }
       if err := os.RemoveAll("docs"); err != nil {
          panic(err)
       }
       if err := os.Rename(temp, "docs"); err != nil {
+         panic(err)
+      }
+      if err := os.WriteFile("docs/.nojekyll", nil, 0666); err != nil {
          panic(err)
       }
    } else {
