568e4aaca80daa1a164447d3eb609e925260f95a
3052 <spen+3052@protonmail.com>
2024-04-08 22:18:03 -0500

    new file:   2024-04/android-apps/android-apps.go

---
+++ 2024-04/android-apps/android-apps.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+   "154.pages.dev/encoding"
+   "154.pages.dev/google/play"
+   "fmt"
+   "os"
+   "slices"
+   "time"
+)
+
+var apps = []application{
+   {id: "au.com.stan.and"},
+   {id: "com.amcplus.amcfullepisodes"},
+   {id: "com.cbs.app"},
+   {id: "com.hulu.plus"},
+   {id: "com.mubi"},
+   {id: "com.nbcuni.nbc"},
+   {id: "com.peacocktv.peacockandroid"},
+   {id: "com.plexapp.android"},
+   {id: "com.roku.remote"},
+}
+
+func main() {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      panic(err)
+   }
+   var token play.RefreshToken
+   token.Data, err = os.ReadFile(home + "/google-play/token.txt")
+   if err != nil {
+      panic(err)
+   }
+   token.Unmarshal()
+   var detail play.Details
+   if err := detail.Token.Refresh(token); err != nil {
+      panic(err)
+   }
+   detail.Checkin.Data, err = os.ReadFile(home + "/google-play/x86.bin")
+   if err != nil {
+      panic(err)
+   }
+   detail.Checkin.Unmarshal()
+   for i, app := range apps {
+      fmt.Println(app)
+      detail.Details(app.id, false)
+      apps[i].installs, _ = detail.Downloads()
+      apps[i].name, _ = detail.Name()
+      time.Sleep(99 * time.Millisecond)
+   }
+   slices.SortFunc(apps, func(a, b application) int {
+      return int(b.installs - a.installs)
+   })
+   for _, app := range apps {
+      fmt.Println(app)
+   }
+}
+
+func (a application) String() string {
+   var b []byte
+   b = fmt.Append(b, encoding.Cardinal(a.installs))
+   b = append(b, ' ')
+   b = append(b, a.name...)
+   return string(b)
+}
+
+type application struct {
+   id string
+   name string
+   installs uint64
+}
--- 2024-04/go.mod
+++ 2024-04/go.mod
@@ -1,3 +1,11 @@
 module blog
 
 go 1.22.0
+
+require 154.pages.dev/google v1.4.2
+
+require (
+	154.pages.dev/encoding v1.18.8 // indirect
+	154.pages.dev/protobuf v1.2.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
