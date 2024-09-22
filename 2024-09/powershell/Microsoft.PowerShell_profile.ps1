ee1032cb50183c90bd6d6c40ea3debe66da3007f
3052 <spen+3052@protonmail.com>
2024-09-22 07:52:25 -0500

    deleted:    2024-09/palgate/readme.md

--- 2024-09/powershell/Microsoft.PowerShell_profile.ps1
+++ 2024-09/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,18 @@
+## 2024-9-21
+
+Set-PSReadLineOption -AddToHistoryHandler $null
+$env:RIPGREP_CONFIG_PATH = 'C:\Users\Steven\ripgrep.txt'
+$MaximumHistoryCount = 9999
+### `git diff` unicode
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+### git commit -v
+$env:EDITOR = 'gvim'
+
+Set-PSReadLineKeyHandler Ctrl+UpArrow {
+   Set-Location ..
+   [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+}
+
 # 2024-9-10
 Get-Alias | Remove-Alias -Force
 Set-PSReadLineOption -PredictionSource None
@@ -14,12 +29,6 @@ $env:PATH = @(
    'D:\vim'
 ) -Join ';'
 
-# 2023-06-18
-$env:RIPGREP_CONFIG_PATH = 'C:\Users\Steven\ripgrep.txt'
-
-# 2023-06-06
-$MaximumHistoryCount = 9999
-
 # 2023-05-10
 $env:LESS = -join @(
    # Quit if entire file fits on first screen.
@@ -31,17 +40,3 @@ $env:LESS = -join @(
    # Ignore case in searches that do not contain uppercase.
    'i'
 )
-
-# 2023-05-10
-Set-PSReadLineKeyHandler Ctrl+UpArrow {
-   Set-Location ..
-   [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-}
-
-# 2023-05-10
-# `git diff` unicode
-[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
-
-# 2023-05-10
-# git commit -v
-$env:EDITOR = 'gvim'
