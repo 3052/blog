a810c7dbbf830b4c4025daf4a1e908c2ac20e8c4
3052 <spen+3052@protonmail.com>
2024-10-29 18:41:11 -0500

    new file:   2024-10/encoding/encoding_test.go

---
+++ 2024-10/encoding/encoding_test.go
@@ -0,0 +1,66 @@
+package encoding
+
+import (
+   "bytes"
+   "encoding/asn1"
+   "encoding/gob"
+   "encoding/json"
+   "testing"
+)
+
+type greeting struct {
+   Hello string
+   World string
+}
+
+func BenchmarkJson(b *testing.B) {
+   for i := range b.N {
+      data, err := json.Marshal(greeting{"hello", "world"})
+      if err != nil {
+         b.Fatal(err)
+      }
+      if i == 0 {
+         b.Log(len(data))
+      }
+      var greet greeting
+      err = json.Unmarshal(data, &greet)
+      if err != nil {
+         b.Fatal(err)
+      }
+   }
+}
+
+func BenchmarkAsn1(b *testing.B) {
+   for i := range b.N {
+      data, err := asn1.Marshal(greeting{"hello", "world"})
+      if err != nil {
+         b.Fatal(err)
+      }
+      if i == 0 {
+         b.Log(len(data))
+      }
+      var greet greeting
+      _, err = asn1.Unmarshal(data, &greet)
+      if err != nil {
+         panic(err)
+      }
+   }
+}
+
+func BenchmarkGob(b *testing.B) {
+   for i := range b.N {
+      data := &bytes.Buffer{}
+      err := gob.NewEncoder(data).Encode(greeting{"hello", "world"})
+      if err != nil {
+         b.Fatal(err)
+      }
+      if i == 0 {
+         b.Log(data.Len())
+      }
+      var greet greeting
+      err = gob.NewDecoder(data).Decode(&greet)
+      if err != nil {
+         b.Fatal(err)
+      }
+   }
+}
