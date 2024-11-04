5a83cca729075508b8bc44bffcb01138d07c611b
3052 <spen+3052@protonmail.com>
2024-11-04 05:24:11 -0600

    +func (g *git_board) New() error {

--- 2024-11/git-board/git-board.go
+++ 2024-11/git-board/git-board.go
@@ -9,8 +9,79 @@ import (
    "time"
 )
 
+func (g *git_board) New() error {
+   cmd := exec.Command("git", "add", ".")
+   fmt.Println(cmd.Args)
+   err := cmd.Run()
+   if err != nil {
+      return err
+   }
+   cmd = exec.Command("git", "diff", "--cached", "--numstat")
+   fmt.Println(cmd.Args)
+   text, err := cmd.Output()
+   if err != nil {
+      return err
+   }
+   // split fails on empty string
+   for _, line := range strings.FieldsFunc(string(text), lines) {
+      var add, del int
+      // binary files will be "- - hello.txt", so ignore error
+      fmt.Sscan(line, &add, &del)
+      // Add
+      g.Add += add
+      // Delete
+      g.Delete += del
+      // Change
+      g.Change++
+   }
+   g.Target = 100
+   if g.Add >= g.Target {
+      g.AddStatus = pass
+   } else {
+      g.AddStatus = fail
+   }
+   if g.Delete >= g.Target {
+      g.DeleteStatus = pass
+   } else {
+      g.DeleteStatus = fail
+   }
+   if g.Change >= g.Target {
+      g.ChangeStatus = pass
+   } else {
+      g.ChangeStatus = fail
+   }
+   // Then
+   then, err := get_then()
+   if err != nil {
+      return err
+   }
+   g.Then = then
+   // Now
+   g.Now = time.Now().AddDate(0, 0, -1).String()[:10]
+   if g.Then <= g.Now {
+      g.DateStatus = pass
+   } else {
+      g.DateStatus = fail
+   }
+   return nil
+}
+
+type git_board struct {
+   Add int
+   AddStatus string
+   Delete int
+   DeleteStatus string
+   Change int
+   ChangeStatus string
+   Target int
+   Then string
+   Now string
+   DateStatus string
+}
+
 func main() {
-   board, err := new_git_board()
+   var board git_board
+   err := board.New()
    if err != nil {
       panic(err)
    }
@@ -36,85 +107,16 @@ func get_then() (string, error) {
    return string(text), nil
 }
 
-func new_git_board() (*git_board, error) {
-   cmd := exec.Command("git", "add", ".")
-   fmt.Println(cmd.Args)
-   err := cmd.Run()
-   if err != nil {
-      return nil, err
-   }
-   cmd = exec.Command("git", "diff", "--cached", "--numstat")
-   fmt.Println(cmd.Args)
-   text, err := cmd.Output()
-   if err != nil {
-      return nil, err
-   }
-   var board git_board
-   // split fails on empty string
-   for _, line := range strings.FieldsFunc(string(text), lines) {
-      var add, del int
-      // binary files will be "- - hello.txt", so ignore error
-      fmt.Sscan(line, &add, &del)
-      // Add
-      board.Add += add
-      // Delete
-      board.Delete += del
-      // Change
-      board.Change++
-   }
-   if board.Add >= 99 {
-      board.AddStatus = pass
-   } else {
-      board.AddStatus = fail
-   }
-   if board.Delete >= 99 {
-      board.DeleteStatus = pass
-   } else {
-      board.DeleteStatus = fail
-   }
-   if board.Change >= 99 {
-      board.ChangeStatus = pass
-   } else {
-      board.ChangeStatus = fail
-   }
-   // Then
-   then, err := get_then()
-   if err != nil {
-      return nil, err
-   }
-   board.Then = then
-   // Now
-   board.Now = time.Now().AddDate(0, 0, -1).String()[:10]
-   if board.Then <= board.Now {
-      board.DateStatus = pass
-   } else {
-      board.DateStatus = fail
-   }
-   return &board, nil
-}
-
 const (
    fail = "\x1b[30;101m Fail \x1b[m"
    pass = "\x1b[30;102m Pass \x1b[m"
 )
 
 const format =
-   "{{ .AddStatus }} additions\ttarget:99\tactual:{{ .Add }}\n" +
-   "{{ .DeleteStatus }} deletions\ttarget:99\tactual:{{ .Delete }}\n" +
-   "{{ .ChangeStatus }} changed files\ttarget:99\tactual:{{ .Change }}\n" +
+   "{{ .AddStatus }} additions\ttarget:{{ .Target }}\tactual:{{ .Add }}\n" +
+   "{{ .DeleteStatus }} deletions\ttarget:{{ .Target }}\tactual:{{ .Delete }}\n" +
+   "{{ .ChangeStatus }} changed files\ttarget:{{ .Target}}\tactual:{{ .Change }}\n" +
    "{{ .DateStatus }} last commit\ttarget:{{ .Then }}\tactual:{{ .Now }}\n"
-
-type git_board struct {
-   Add int
-   AddStatus string
-   Delete int
-   DeleteStatus string
-   Change int
-   ChangeStatus string
-   Then string
-   Now string
-   DateStatus string
-}
 
 func lines(r rune) bool {
    return r == '\n'
