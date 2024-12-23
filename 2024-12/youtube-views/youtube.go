438d92412ff99f57962a56d877ca63fb3926a7bf
3052 <spen+3052@protonmail.com>
2024-12-23 05:12:36 -0600

    deleted:    2024-12/youtube-views/main.go

--- 2024-12/youtube-views/youtube.go
+++ 2024-12/youtube-views/youtube.go
@@ -3,10 +3,62 @@ package main
 import (
    "bytes"
    "encoding/json"
+   "flag"
+   "fmt"
    "net/http"
    "net/url"
    "path"
+   "strconv"
    "time"
+)
+
+func views_per_year(play *Player) (string, error) {
+   views := func() float64 {
+      date := play.Microformat.PlayerMicroformatRenderer.PublishDate.Time
+      fmt.Println(date)
+      years := time.Since(date).Hours() / 24 / 365
+      return float64(play.VideoDetails.ViewCount) / years
+   }()
+   var data []byte
+   if views >= 10_000_000 {
+      data = append(data, red...)
+      data = append(data, " FAIL "...)
+   } else {
+      data = append(data, green...)
+      data = append(data, " PASS "...)
+   }
+   data = append(data, reset...)
+   data = append(data, "   "...)
+   data = strconv.AppendFloat(data, views, 'f', 0, 64)
+   data = append(data, "   "...)
+   data = append(data, play.VideoDetails.VideoId...)
+   return string(data), nil
+}
+
+func main() {
+   var tube InnerTube
+   tube.Context.Client.ClientName = web
+   flag.StringVar(&tube.VideoId, "v", "", "video ID")
+   flag.Parse()
+   if tube.VideoId != "" {
+      play, err := tube.Player()
+      if err != nil {
+         panic(err)
+      }
+      views, err := views_per_year(play)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Println(views)
+   } else {
+      flag.Usage()
+   }
+}
+
+const (
+   reset = "\x1b[m"
+   green = "\x1b[30;102m"
+   red = "\x1b[30;101m"
 )
 
 type Player struct {
