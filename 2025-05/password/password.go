c7934df3f7a5f2bee4f98e5e484d2935816caa95
367 <367@tuta.io>
2025-05-03 09:04:25 -0500

    renamed:    2025-04/clean-home/clean-home.go -> 2025-05/clean-home/clean-home.go

--- 2025-05/password/password.go
+++ 2025-05/password/password.go
@@ -10,35 +10,6 @@ import (
    "time"
 )
 
-func main() {
-   var user1 user
-   err := user1.New()
-   if err != nil {
-      panic(err)
-   }
-   for key, value := range user1 {
-      const day = 24 * time.Hour
-      if time.Since(value.Date) >= 365*day {
-         fmt.Println("time.Since(value.Date) >= 365*day")
-         fmt.Println(key)
-         fmt.Println(&value)
-         return
-         
-      }
-   }
-   contains := flag.String("c", "", "contains")
-   equal := flag.String("e", "", "equal")
-   flag.Parse()
-   switch {
-   case *contains != "":
-      user1.contains(*contains)
-   case *equal != "":
-      user1.equal(*equal)
-   default:
-      flag.Usage()
-   }
-}
-
 func (u *user) New() error {
    home, err := os.UserHomeDir()
    if err != nil {
@@ -61,33 +32,68 @@ func (i *info) String() string {
    return string(b)
 }
 
+func main() {
+   var user1 user
+   err := user1.New()
+   if err != nil {
+      panic(err)
+   }
+   const day = 24 * time.Hour
+   for key, values := range user1 {
+      for _, value := range values {
+         if time.Since(value.Date) >= 365*day {
+            fmt.Println("time.Since(value.Date) >= 365*day")
+            fmt.Println(key)
+            fmt.Println(&value)
+            return
+         }
+      }
+   }
+   contains := flag.String("c", "", "contains")
+   equal := flag.String("e", "", "equal")
+   flag.Parse()
+   switch {
+   case *contains != "":
+      user1.contains(*contains)
+   case *equal != "":
+      user1.equal(*equal)
+   default:
+      flag.Usage()
+   }
+}
+
+func (u user) contains(data string) {
+   var line bool
+   for key, values := range u {
+      if strings.Contains(key, data) {
+         for _, value := range values {
+            if line {
+               fmt.Println()
+            } else {
+               line = true
+            }
+            fmt.Print(key, "\n", &value, "\n")
+         }
+      }
+   }
+}
+
 type info struct {
    Date     time.Time
    Password string
    Username string
 }
 
-type user map[string]info
+type user map[string][]info
 
 func (u user) equal(data string) {
-   info1 := u[data]
-   fmt.Print(info1.Username)
-   if info1.Password != "" {
-      // go.dev/pkg/net/url?m=old#PathEscape
-      fmt.Print(":", info1.Password)
-   }
-}
-
-func (u user) contains(data string) {
-   var line bool
-   for key, value := range u {
-      if strings.Contains(key, data) {
-         if line {
-            fmt.Println()
-         } else {
-            line = true
-         }
-         fmt.Print(key, "\n", &value, "\n")
+   values, ok := u[data]
+   if ok {
+      value := values[0]
+      fmt.Print(value.Username)
+      if value.Password != "" {
+         // go.dev/pkg/net/url?m=old#PathEscape
+         fmt.Print(":", value.Password)
       }
    }
 }
