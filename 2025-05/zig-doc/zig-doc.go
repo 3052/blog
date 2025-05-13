df255f64ef38753c17ebb5de47ae0422ffac696d
367 <367@tuta.io>
2025-05-13 00:01:26 -0500

    +func main() {

--- 2025-05/zig-doc/zig-doc.go
+++ 2025-05/zig-doc/zig-doc.go
@@ -9,6 +9,21 @@ import (
    "os/exec"
 )
 
+func main() {
+   from := flag.String("f", "", "from address")
+   // need localhost to avoid security alert
+   to := flag.String("t", "localhost:8080", "to address")
+   flag.Parse()
+   if *from != "" {
+      err := serve(*from, *to)
+      if err != nil {
+         panic(err)
+      }
+   } else {
+      flag.Usage()
+   }
+}
+
 func get(address string) error {
    resp, err := http.Get(address)
    if err != nil {
@@ -45,19 +60,4 @@ func serve(from, to string) error {
    return http.ListenAndServe(
       to, http.FileServer(http.Dir("docs")),
    )
-}
-
-func main() {
-   from := flag.String("f", "", "from address")
-   // need localhost to avoid security alert
-   to := flag.String("t", "localhost:8080", "to address")
-   flag.Parse()
-   if *from != "" {
-      err := serve(*from, *to)
-      if err != nil {
-         panic(err)
-      }
-   } else {
-      flag.Usage()
-   }
 }
