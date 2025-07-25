3e7916cd11261b525bd2d7a51d14b064fe04f825
367 <367@tuta.io>
2025-07-25 00:32:46 -0500

    deleted:    2025-07/chatBot/chatGpt/chatGpt.go

--- 2025-07/chatBot/chatGpt/chatGpt_test.go
+++ 2025-07/chatBot/chatGpt/chatGpt_test.go
@@ -7,59 +7,95 @@ import (
    "testing"
 )
 
+const initialization = 0
+
 var tests = []struct {
    name           string
-   representation []representation
+   representation []representationA
 }{
    {
-      name: "criterion.txt",
-      representation: []representation{
-         {
-            content_type: type_text,
-            id:           "subs-7433271",
-            length:       1,
-            url:          prefix + "texttrack/sub/7433271.vtt?pathsig=8c953e4f~UO056QMhmjVj394TCzXUSJJ4GI4BcpMoXktkwXsYSjw&r=dXMtY2VudHJhbDE%3D",
-         },
+      name: "canal.mpd",
+      representation: []representationA{
          {
             content_type: type_video,
-            id:           "video-888d2bc7-75b5-4264-bf57-08e3dc24ecbb",
-            length: func() int {
-               initialization := 1
-               media := 1 + 1114 + 1
-               return initialization + media
-            }(),
-            url: prefix + "drm/cenc,derived,325579370,e4576465a745213f336c1ef1bf5d513e/remux/avf/888d2bc7-75b5-4264-bf57-08e3dc24ecbb/segment.mp4?pathsig=8c953e4f~vEyD7FR7NMtgBhRbRGol6tYRL0pVp7AQxjE5pUlKliI&r=dXMtY2VudHJhbDE%3D&sid=1116&st=video",
+            id:           "video=3399914",
+            length:       initialization + 1 + 1332 + 1,
+            url:          prefix + "dash/appletvcz_A007300100102_2464C3BF9652075492E7CF48A400F243_HD-video=3399914-4798800.dash?serviceid=298f95e1bf91361258c44a2b1f4a2425",
+         },
+         {
+            content_type: type_image,
+            id:           "thumbnail",
+            length:       80,
+            url:          prefix + "dash/thumbnail/tile_80.jpeg?serviceid=298f95e1bf91361258c44a2b1f4a2425",
          },
       },
    },
    {
-      name: "molotov.txt",
-      representation: []representation{
+      name: "criterion.mpd",
+      representation: []representationA{
+         {
+            content_type: type_video,
+            id:           "video-31e3417b-d163-45ba-9d7d-c356ef7aca57",
+            length:       initialization + 1084,
+            url:          prefix + "drm/cenc,derived,1039101898,70c108dd0a77b48f8738a37ed402d1e3/range/prot/aW5pdF9yYW5nZT0wLTgwNCZyYW5nZT0xODYzNTk5MTk1LTE4NjM4MTU0Mzk/avf/31e3417b-d163-45ba-9d7d-c356ef7aca57.mp4?init_range=0-804&pathsig=8c953e4f~H4likJuUcpljeDjz1r1ky7d3kH28faTdejTPfwh_5DY&r=dXMtZWFzdDE%3D&range=1863599195-1863815439",
+         },
          {
             content_type: type_text,
-            id:           "3=1000",
-            length: func() int {
-               initialization := 1
-               media := 3339
-               return initialization + media
-            }(),
-            url: prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-3=1000-3339.m4s",
+            id:           "subs-202936084",
+            length:       1,
+            url:          prefix + "texttrack/sub/202936084.vtt?pathsig=8c953e4f~VmhEmE4Ge9n9S9R4gdPqJo02oUhoM8-o1o0iBVypx3k&r=dXMtY2VudHJhbDE%3D",
          },
+      },
+   },
+   {
+      name: "max.mpd",
+      representation: []representationA{
+         {
+            content_type: type_video,
+            id:           "v31",
+            length:       initialization + 1,
+            url:          prefix + "v/1_584f52/v31.mp4",
+         },
+         {
+            content_type: type_image,
+            id:           "images_1",
+            length:       initialization + 1864,
+            url:          prefix + "i/1_6c0f17/images_1_00001863.jpg",
+         },
+         {
+            content_type: type_text,
+            id:           "t2",
+            length:       initialization + 8,
+            url:          prefix + "t/0_d2d294/t2/8.vtt",
+         },
+      },
+   },
+   {
+      name: "molotov.mpd",
+      representation: []representationA{
          {
             content_type: type_video,
             id:           "video=4800000",
-            length: func() int {
-               initialization := 1
-               media := 3555
-               return initialization + media
-            }(),
-            url: prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-video=4800000-3555.m4s",
+            length:       initialization + 3555,
+            url:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-video=4800000-3555.m4s",
+         },
+         {
+            content_type: type_text,
+            id:           "3=1000",
+            length:       initialization + 3339,
+            url:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-3=1000-3339.m4s",
          },
       },
    },
    {
-      name: "paramount.txt",
-      representation: []representation{
+      name: "paramount.mpd",
+      representation: []representationA{
+         {
+            content_type: type_video,
+            id:           "5",
+            url:          prefix + "TPIR_0722_100824_2997DF_1920x1080_178_2CH_PRORESHQ_2CH_2939373_4500/seg_571.m4s",
+            length:       initialization + 539 + 1 + 1 + 29 + 1,
+         },
          {
             content_type: type_image,
             id:           "thumb_320x180",
@@ -69,83 +105,19 @@ var tests = []struct {
          {
             content_type: type_text,
             id:           "8",
-            length: func() int {
-               initialization := 1
-               media := 540 + 1 + 22
-               return initialization + media
-            }(),
-            url: prefix + "TPIR_0722_2997_2CH_DF_1728406422/seg_563.m4s",
-         },
-         {
-            content_type: type_video,
-            id:           "5",
-            url:          prefix + "TPIR_0722_100824_2997DF_1920x1080_178_2CH_PRORESHQ_2CH_2939373_4500/seg_571.m4s",
-            length: func() int {
-               initialization := 1
-               media := 539 + 1 + 1 + 29 + 1
-               return initialization + media
-            }(),
+            length:       initialization + 540 + 1 + 22,
+            url:          prefix + "TPIR_0722_2997_2CH_DF_1728406422/seg_563.m4s",
          },
       },
    },
    {
-      name: "pluto.txt",
-      representation: []representation{
-         {
-            content_type: type_text,
-            id:           "7",
-            length: func() int {
-               initialization := 1
-               media := 1 + 1153
-               return initialization + media
-            }(),
-            url: prefix + "text/en-cc/01154.m4s",
-         },
-         {
-            content_type: type_video,
-            id:           "5",
-            length: func() int {
-               initialization := 1
-               media := 1 + 1205 + 1
-               return initialization + media
-            }(),
-            url: prefix + "video/1080p-4500/01207.m4s",
-         },
-      },
-   },
-   {
-      name: "rakuten.txt",
-      representation: []representation{
+      name: "rakuten.mpd",
+      representation: []representationA{
          {
             content_type: type_video,
             id:           "video-avc1-6",
             length:       1,
             url:          "https://prod-avod-pmd-cdn77.cdn.rakuten.tv/3/1/8/318f7ece69afcfe3e96de31be6b77272-mc-0-164-0-0_DS2BB/video-avc1-6.ismv?streaming_id=630ed6ed-1137-473c-8858-23ba59d12675&st_country=CZ,AT,DE,PL,SK&st_valid=1752245624&secure=PcDPRLtVJ_-tDsEPMD5Hzg==,1752267224",
-         },
-      },
-   },
-   {
-      name: "rtbf.txt",
-      representation: []representation{
-         {
-            content_type: type_video,
-            id:           "video=5200000",
-            length: func() int {
-               initialization := 1
-               media := 1 + 1011 + 1
-               return initialization + media
-            }(),
-            url: prefix + "dash/vod-idx-2-video=5200000-3497472.dash",
-         },
-         {
-            content_type: type_text,
-            id:           "textstream_fra_1=1000",
-            length: func() int {
-               initialization := 1
-               media := 1 + 974 + 1
-               return initialization + media
-            }(),
-            url: prefix + "dash/vod-idx-2-textstream_fra_1=1000-505440000.dash",
          },
       },
    },
@@ -196,7 +168,7 @@ const (
    type_video
 )
 
-type representation struct {
+type representationA struct {
    id           string
    length       int
    url          string
