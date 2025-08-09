102ed6d75dda817165f11ca496572157383dae03
367 <367@tuta.io>
2025-08-09 18:27:57 -0500

    deleted:    2025-08/chatBot/chatGpt-4o/chatBot.go

--- 2025-08/chatBot/qwen/readme.md
+++ 2025-08/chatBot/qwen/readme.md
@@ -1,71 +1,10 @@
 # qwen
 
-- https://app.fireworks.ai/models/fireworks/qwen3-coder-480b-a35b-instruct
-- https://wikipedia.org/wiki/Qwen
+https://app.fireworks.ai/models/fireworks/qwen3-coder-480b-a35b-instruct
 
-## prompt 1, 36s
+## prompt 1
 
 return the full Go script that:
 - Reads an MPEG-DASH MPD XML file path from the CLI: `go run main.go <mpd_file_path>`
 - Uses `http://test.test/test.mpd` as the initial MPD URL for resolving all relative BaseURLs
 - Outputs a JSON object mapping each `Representation@id` to a list of fully resolved segment URLs
-
-## prompt 2, 1m3s
-
-how many times do I need to say it, BaseURL field is string not slice, do as I
-say
-
-## prompt 3, 20s
-
-all the code should be in a single code block
-
-## prompt 4, 26s
-
-SegmentTemplate can be child of Representation or AdaptationSet
-
-## prompt 5, 30s
-
-support SegmentTimeline
-
-## prompt 6, 28s
-
-replace `$RepresentationID$`
-
-## prompt 7, 33s
-
-include SegmentTemplate@initialization if it exists
-
-## prompt 8, 33s
-
-support SegmentTemplate@endNumber
-
-## prompt 9, 32s
-
-with SegmentList, include Initialization@sourceURL if it exists
-
-## prompt 10, 42s
-
-Append segments for the same Representation ID if it appears in multiple
-Periods
-
-## prompt 11, 43s
-
-If both `SegmentTimeline` and `endNumber` are missing, but `duration` and
-`timescale` are present, calculate the number of segments using
-`ceil(PeriodDurationInSeconds * timescale / duration)`
-
-## prompt 12, 36s
-
-Default `timescale` to `1` if missing
-
-## prompt 13, 40s
-
-replace input like `$Number%08d$`
-
-## prompt 14, 38s
-
-SegmentTemplate@startNumber is `1` if missing
-
-## prompt 15, 4m27s
-
-MPD@BaseURL can be different from MPD URL
--- 2025-08/chatBot/z.ai/chatBot_test.go
+++ 2025-08/chatBot/z.ai/chatBot_test.go
@@ -23,13 +23,15 @@ var tests = []struct {
             content_type: type_video,
             id:           "video=3399914",
             length:       initialization + 1 + 1332 + 1,
-            url:          prefix + "dash/appletvcz_A007300100102_2464C3BF9652075492E7CF48A400F243_HD-video=3399914-4798800.dash?serviceid=298f95e1bf91361258c44a2b1f4a2425",
+            start:        prefix + "dash/appletvcz_A007300100102_2464C3BF9652075492E7CF48A400F243_HD-video=3399914.dash?serviceid=298f95e1bf91361258c44a2b1f4a2425",
+            end:          prefix + "dash/appletvcz_A007300100102_2464C3BF9652075492E7CF48A400F243_HD-video=3399914-4798800.dash?serviceid=298f95e1bf91361258c44a2b1f4a2425",
          },
          {
             content_type: type_image,
             id:           "thumbnail",
             length:       80,
-            url:          prefix + "dash/thumbnail/tile_80.jpeg?serviceid=298f95e1bf91361258c44a2b1f4a2425",
+            start:        prefix + "dash/thumbnail/tile_1.jpeg?serviceid=298f95e1bf91361258c44a2b1f4a2425",
+            end:          prefix + "dash/thumbnail/tile_80.jpeg?serviceid=298f95e1bf91361258c44a2b1f4a2425",
          },
       },
    },
@@ -40,13 +42,15 @@ var tests = []struct {
             content_type: type_video,
             id:           "video-31e3417b-d163-45ba-9d7d-c356ef7aca57",
             length:       initialization + 1084,
-            url:          prefix + "drm/cenc,derived,1039101898,70c108dd0a77b48f8738a37ed402d1e3/range/prot/aW5pdF9yYW5nZT0wLTgwNCZyYW5nZT0xODYzNTk5MTk1LTE4NjM4MTU0Mzk/avf/31e3417b-d163-45ba-9d7d-c356ef7aca57.mp4?init_range=0-804&pathsig=8c953e4f~H4likJuUcpljeDjz1r1ky7d3kH28faTdejTPfwh_5DY&r=dXMtZWFzdDE%3D&range=1863599195-1863815439",
+            start:        prefix + "drm/cenc,derived,1039101898,70c108dd0a77b48f8738a37ed402d1e3/range/prot/aW5pdF9yYW5nZT0wLTgwNCZyYW5nZT0wLTgwNA/avf/31e3417b-d163-45ba-9d7d-c356ef7aca57.mp4?init_range=0-804&pathsig=8c953e4f~0yotwnRXHn_MAwePXLjUCgWFaE37toTPlqP5_COJ7Fg&r=dXMtZWFzdDE%3D&range=0-804",
+            end:          prefix + "drm/cenc,derived,1039101898,70c108dd0a77b48f8738a37ed402d1e3/range/prot/aW5pdF9yYW5nZT0wLTgwNCZyYW5nZT0xODYzNTk5MTk1LTE4NjM4MTU0Mzk/avf/31e3417b-d163-45ba-9d7d-c356ef7aca57.mp4?init_range=0-804&pathsig=8c953e4f~H4likJuUcpljeDjz1r1ky7d3kH28faTdejTPfwh_5DY&r=dXMtZWFzdDE%3D&range=1863599195-1863815439",
          },
          {
             content_type: type_text,
             id:           "subs-202936084",
             length:       1,
-            url:          prefix + "texttrack/sub/202936084.vtt?pathsig=8c953e4f~VmhEmE4Ge9n9S9R4gdPqJo02oUhoM8-o1o0iBVypx3k&r=dXMtY2VudHJhbDE%3D",
+            start:        prefix + "texttrack/sub/202936084.vtt?pathsig=8c953e4f~VmhEmE4Ge9n9S9R4gdPqJo02oUhoM8-o1o0iBVypx3k&r=dXMtY2VudHJhbDE%3D",
+            end:          prefix + "texttrack/sub/202936084.vtt?pathsig=8c953e4f~VmhEmE4Ge9n9S9R4gdPqJo02oUhoM8-o1o0iBVypx3k&r=dXMtY2VudHJhbDE%3D",
          },
       },
    },
@@ -57,29 +61,32 @@ var tests = []struct {
             content_type: type_video,
             id:           "v31",
             length:       8, // allow duplicates
-            url:          prefix + "v/1_584f52/v31.mp4",
+            start:        prefix + "v/1_584f52/v31.mp4",
+            end:          prefix + "v/1_584f52/v31.mp4",
          },
          {
             content_type: type_text,
             id:           "t2",
             length:       8,
-            url:          prefix + "t/0_d2d294/t2/8.vtt",
+            start:        prefix + "t/0_d2d294/t2/1.vtt",
+            end:          prefix + "t/0_d2d294/t2/8.vtt",
          },
          {
             content_type: type_image,
             id:           "images_1",
             length: func() int { // allow duplicates
-               media := math.Ceil(1100.34925 / 5)
-               media += math.Ceil(1122.2044166666667 / 5)
-               media += math.Ceil(1104.1864166666664 / 5)
-               media += math.Ceil(1017.5582083333338 / 5)
-               media += math.Ceil(1058.9328749999995 / 5)
-               media += math.Ceil(1011.9692916666672 / 5)
-               media += math.Ceil(994.4934999999996 / 5)
-               media += math.Ceil(1914.2873749999999 / 5)
+               media := math.Ceil(1100.34925 / 5)         // 221, 0-220
+               media += math.Ceil(1122.2044166666667 / 5) // 225, 220-
+               media += math.Ceil(1104.1864166666664 / 5) // 221
+               media += math.Ceil(1017.5582083333338 / 5) // 204
+               media += math.Ceil(1058.9328749999995 / 5) // 212
+               media += math.Ceil(1011.9692916666672 / 5) // 203
+               media += math.Ceil(994.4934999999996 / 5)  // 199
+               media += math.Ceil(1914.2873749999999 / 5) // 383
                return int(media)
             }(),
-            url: prefix + "i/1_6c0f17/images_1_00001863.jpg",
+            start: prefix + "i/1_6c0f17/images_1_00000000.jpg",
+            end:   prefix + "i/1_6c0f17/images_1_00001863.jpg",
          },
       },
    },
@@ -90,13 +97,15 @@ var tests = []struct {
             content_type: type_video,
             id:           "video=4800000",
             length:       initialization + 3555,
-            url:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-video=4800000-3555.m4s",
+            start:        prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-video=4800000.dash",
+            end:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-video=4800000-3555.m4s",
          },
          {
             content_type: type_text,
             id:           "3=1000",
             length:       initialization + 3339,
-            url:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-3=1000-3339.m4s",
+            start:        prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-3=1000.dash",
+            end:          prefix + "dash/32e3c47902de4911dca77b0ad73e9ac34965a1d8-3=1000-3339.m4s",
          },
       },
    },
@@ -106,20 +115,23 @@ var tests = []struct {
          {
             content_type: type_video,
             id:           "5",
-            url:          prefix + "TPIR_0722_100824_2997DF_1920x1080_178_2CH_PRORESHQ_2CH_2939373_4500/seg_571.m4s",
             length:       9*initialization + 539 + 1 + 1 + 29 + 1,
+            start:        prefix + "TPIR_0722_100824_2997DF_1920x1080_178_2CH_PRORESHQ_2CH_2939373_4500/init.m4v",
+            end:          prefix + "TPIR_0722_100824_2997DF_1920x1080_178_2CH_PRORESHQ_2CH_2939373_4500/seg_571.m4s",
          },
          {
             content_type: type_image,
             id:           "thumb_320x180",
             length:       11,
-            url:          prefix + "thumb_320x180/tile_11.jpg",
+            start:        prefix + "thumb_320x180/tile_1.jpg",
+            end:          prefix + "thumb_320x180/tile_11.jpg",
          },
          {
             content_type: type_text,
             id:           "8",
             length:       9*initialization + 540 + 1 + 22,
-            url:          prefix + "TPIR_0722_2997_2CH_DF_1728406422/seg_563.m4s",
+            start:        prefix + "TPIR_0722_2997_2CH_DF_1728406422/vtt_init.m4v",
+            end:          prefix + "TPIR_0722_2997_2CH_DF_1728406422/seg_563.m4s",
          },
       },
    },
@@ -130,7 +142,8 @@ var tests = []struct {
             content_type: type_video,
             id:           "video-avc1-6",
             length:       1,
-            url:          "https://prod-avod-pmd-cdn77.cdn.rakuten.tv/3/1/8/318f7ece69afcfe3e96de31be6b77272-mc-0-164-0-0_DS2BB/video-avc1-6.ismv?streaming_id=630ed6ed-1137-473c-8858-23ba59d12675&st_country=CZ,AT,DE,PL,SK&st_valid=1752245624&secure=PcDPRLtVJ_-tDsEPMD5Hzg==,1752267224",
+            start:        "https://prod-avod-pmd-cdn77.cdn.rakuten.tv/3/1/8/318f7ece69afcfe3e96de31be6b77272-mc-0-164-0-0_DS2BB/video-avc1-6.ismv?streaming_id=630ed6ed-1137-473c-8858-23ba59d12675&st_country=CZ,AT,DE,PL,SK&st_valid=1752245624&secure=PcDPRLtVJ_-tDsEPMD5Hzg==,1752267224",
+            end:          "https://prod-avod-pmd-cdn77.cdn.rakuten.tv/3/1/8/318f7ece69afcfe3e96de31be6b77272-mc-0-164-0-0_DS2BB/video-avc1-6.ismv?streaming_id=630ed6ed-1137-473c-8858-23ba59d12675&st_country=CZ,AT,DE,PL,SK&st_valid=1752245624&secure=PcDPRLtVJ_-tDsEPMD5Hzg==,1752267224",
          },
       },
    },
@@ -139,7 +152,7 @@ var tests = []struct {
 func Test(t *testing.T) {
    log.SetFlags(log.Ltime)
    for _, testVar := range tests {
-      data, err := output("go", "run", ".", folder + testVar.name)
+      data, err := output("go", "run", ".", folder+testVar.name)
       if err != nil {
          t.Fatal(string(data))
       }
@@ -157,9 +170,15 @@ func Test(t *testing.T) {
                "fail", len(representB),
             )
          }
-         if representB[len(representB)-1] != representA.url {
+         if representB[0] != representA.start {
             t.Fatal(
-               "\npass", representA.url,
+               "\npass", representA.start,
+               "\nfail", representB[0],
+            )
+         }
+         if representB[len(representB)-1] != representA.end {
+            t.Fatal(
+               "\npass", representA.end,
                "\nfail", representB[len(representB)-1],
             )
          }
@@ -182,10 +201,11 @@ const (
 )
 
 type representationA struct {
+   content_type content_type
    id           string
    length       int
-   url          string
-   content_type content_type
+   start        string
+   end          string
 }
 
 const prefix = "http://test.test/"
