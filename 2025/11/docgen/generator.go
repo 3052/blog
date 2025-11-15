b08bb1414789238c6045d567cd76f99d0481d438
27 <27@riseup.net>
2025-11-15 01:38:45 -0600

    modified:   2025/11/docgen/parser.go

--- 2025/11/docgen/generator.go
+++ 2025/11/docgen/generator.go
@@ -1,53 +1,53 @@
 package docgen
 
 import (
-	"fmt"
-	"html/template"
-	"os"
-	"path/filepath"
-	"strings"
+   "fmt"
+   "html/template"
+   "os"
+   "path/filepath"
+   "strings"
 )
 
 var funcMap = template.FuncMap{
-	"nl2br": func(text string) template.HTML {
-		return template.HTML(strings.ReplaceAll(strings.TrimSpace(text), "\n", "<br>"))
-	},
-	"cleantag": func(tag string) string {
-		return strings.Trim(tag, "`")
-	},
-	// RESTORED: This function creates an HTML link for the receiver type.
-	"linkifyRecv": func(signature, recvType string) template.HTML {
-		// Create the hyperlink HTML
-		link := fmt.Sprintf(`<a href="#%s">%s</a>`, recvType, recvType)
+   "nl2br": func(text string) template.HTML {
+      return template.HTML(strings.ReplaceAll(strings.TrimSpace(text), "\n", "<br>"))
+   },
+   "cleantag": func(tag string) string {
+      return strings.Trim(tag, "`")
+   },
+   // RESTORED: This function creates an HTML link for the receiver type.
+   "linkifyRecv": func(signature, recvType string) template.HTML {
+      // Create the hyperlink HTML
+      link := fmt.Sprintf(`<a href="#%s">%s</a>`, recvType, recvType)
 
-		// Replace the first occurrence of the receiver type name in the signature.
-		// We use strings.Replace with a count of 1 to avoid mangling return types.
-		// This trusts that the receiver is the first instance of the type name.
-		linkedSignature := strings.Replace(signature, recvType, link, 1)
+      // Replace the first occurrence of the receiver type name in the signature.
+      // We use strings.Replace with a count of 1 to avoid mangling return types.
+      // This trusts that the receiver is the first instance of the type name.
+      linkedSignature := strings.Replace(signature, recvType, link, 1)
 
-		return template.HTML(linkedSignature)
-	},
+      return template.HTML(linkedSignature)
+   },
 }
 
 // Generate function is unchanged from the last working version.
 func Generate(info *DocInfo, outputPath string) error {
-	tmpl, err := template.New("").Funcs(funcMap).ParseFiles("template.html")
-	if err != nil {
-		return fmt.Errorf("FATAL: failed to parse template.html: %w", err)
-	}
+   tmpl, err := template.New("").Funcs(funcMap).ParseFiles("template.html")
+   if err != nil {
+      return fmt.Errorf("FATAL: failed to parse template.html: %w", err)
+   }
 
-	fileName := fmt.Sprintf("%s.html", info.PackageName)
-	filePath := filepath.Join(outputPath, fileName)
+   fileName := fmt.Sprintf("%s.html", info.PackageName)
+   filePath := filepath.Join(outputPath, fileName)
 
-	file, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create output file %s: %w", filePath, err)
-	}
-	defer file.Close()
+   file, err := os.Create(filePath)
+   if err != nil {
+      return fmt.Errorf("failed to create output file %s: %w", filePath, err)
+   }
+   defer file.Close()
 
-	if err := tmpl.ExecuteTemplate(file, "template.html", info); err != nil {
-		return fmt.Errorf("failed to execute template: %w", err)
-	}
+   if err := tmpl.ExecuteTemplate(file, "template.html", info); err != nil {
+      return fmt.Errorf("failed to execute template: %w", err)
+   }
 
-	return nil
+   return nil
 }
--- 2025/11/docgen/parser.go
+++ 2025/11/docgen/parser.go
@@ -1,144 +1,156 @@
 package docgen
 
 import (
-	"bytes"
-	"go/ast"
-	"go/parser"
-	"go/printer"
-	"go/token"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
+   "bytes"
+   "go/ast"
+   "go/parser"
+   "go/printer"
+   "go/token"
+   "log"
+   "os"
+   "path/filepath"
+   "strings"
 )
 
 type DocInfo struct {
-	PackageName    string
-	PackageComment string
-	Structs        []*StructInfo
+   PackageName    string
+   PackageComment string
+   Structs        []*StructInfo
 }
 
 type StructInfo struct {
-	Name    string
-	Comment string
-	Fields  []FieldInfo
-	Methods []MethodInfo
+   Name    string
+   Comment string
+   Fields  []FieldInfo
+   Methods []MethodInfo
 }
 
 type FieldInfo struct {
-	Name    string
-	Type    string
-	Comment string
-	Tag     string
+   Name    string
+   Type    string
+   Comment string
+   Tag     string
 }
 
 // CORRECTED: MethodInfo now holds the clean signature AND the receiver type for linking.
 type MethodInfo struct {
-	Comment   string
-	Signature string
-	RecvType  string
+   Comment   string
+   Signature string
+   RecvType  string
 }
 
 func ParsePackage(path string) (*DocInfo, error) {
-	fset := token.NewFileSet()
-	structMap := make(map[string]*StructInfo)
-	docInfo := &DocInfo{}
-	var files []*ast.File
+   fset := token.NewFileSet()
+   structMap := make(map[string]*StructInfo)
+   docInfo := &DocInfo{}
+   var files []*ast.File
 
-	// Step 1: Manually find and parse each .go file.
-	log.Println("--- PARSER: Step 1: Parsing Go files ---")
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
-	for _, entry := range entries {
-		if !entry.IsDir() && strings.HasSuffix(entry.Name(), ".go") {
-			filePath := filepath.Join(path, entry.Name())
-			log.Printf("Parsing file: %s", filePath)
-			file, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
-			if err != nil {
-				return nil, err
-			}
-			files = append(files, file)
-			if docInfo.PackageName == "" {
-				docInfo.PackageName = file.Name.Name
-				log.Printf("Determined Package Name: '%s'", docInfo.PackageName)
-			}
-		}
-	}
+   // Step 1: Manually find and parse each .go file.
+   log.Println("--- PARSER: Step 1: Parsing Go files ---")
+   entries, err := os.ReadDir(path)
+   if err != nil {
+      return nil, err
+   }
+   for _, entry := range entries {
+      if !entry.IsDir() && strings.HasSuffix(entry.Name(), ".go") {
+         filePath := filepath.Join(path, entry.Name())
+         log.Printf("Parsing file: %s", filePath)
+         file, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
+         if err != nil {
+            return nil, err
+         }
+         files = append(files, file)
+         if docInfo.PackageName == "" {
+            docInfo.PackageName = file.Name.Name
+            log.Printf("Determined Package Name: '%s'", docInfo.PackageName)
+         }
+      }
+   }
 
-	// Step 2: A single pass to process the ASTs.
-	log.Println("--- PARSER: Step 2: Processing AST ---")
-	for _, file := range files {
-		if file.Doc != nil && docInfo.PackageComment == "" {
-			docInfo.PackageComment = file.Doc.Text()
-		}
+   // Step 2: A single pass to process the ASTs.
+   log.Println("--- PARSER: Step 2: Processing AST ---")
+   for _, file := range files {
+      if file.Doc != nil && docInfo.PackageComment == "" {
+         docInfo.PackageComment = file.Doc.Text()
+      }
 
-		ast.Inspect(file, func(n ast.Node) bool {
-			switch decl := n.(type) {
-			case *ast.TypeSpec:
-				if s, ok := decl.Type.(*ast.StructType); ok {
-					structInfo := &StructInfo{Name: decl.Name.Name, Comment: decl.Doc.Text()}
-					// Field parsing is unchanged...
-					for _, field := range s.Fields.List {
-						fieldType := nodeToString(fset, field.Type)
-						if len(field.Names) == 0 {
-							fieldInfo := FieldInfo{Name: fieldType, Type: fieldType}
-							if field.Doc != nil { fieldInfo.Comment = field.Doc.Text() }
-							if field.Tag != nil { fieldInfo.Tag = field.Tag.Value }
-							structInfo.Fields = append(structInfo.Fields, fieldInfo)
-						} else {
-							for _, name := range field.Names {
-								fieldInfo := FieldInfo{Name: name.Name, Type: fieldType}
-								if field.Doc != nil { fieldInfo.Comment = field.Doc.Text() }
-								if field.Tag != nil { fieldInfo.Tag = field.Tag.Value }
-								structInfo.Fields = append(structInfo.Fields, fieldInfo)
-							}
-						}
-					}
-					docInfo.Structs = append(docInfo.Structs, structInfo)
-					structMap[structInfo.Name] = structInfo
-				}
+      ast.Inspect(file, func(n ast.Node) bool {
+         switch decl := n.(type) {
+         case *ast.TypeSpec:
+            if s, ok := decl.Type.(*ast.StructType); ok {
+               structInfo := &StructInfo{Name: decl.Name.Name, Comment: decl.Doc.Text()}
+               // Field parsing is unchanged...
+               for _, field := range s.Fields.List {
+                  fieldType := nodeToString(fset, field.Type)
+                  if len(field.Names) == 0 {
+                     fieldInfo := FieldInfo{Name: fieldType, Type: fieldType}
+                     if field.Doc != nil {
+                        fieldInfo.Comment = field.Doc.Text()
+                     }
+                     if field.Tag != nil {
+                        fieldInfo.Tag = field.Tag.Value
+                     }
+                     structInfo.Fields = append(structInfo.Fields, fieldInfo)
+                  } else {
+                     for _, name := range field.Names {
+                        fieldInfo := FieldInfo{Name: name.Name, Type: fieldType}
+                        if field.Doc != nil {
+                           fieldInfo.Comment = field.Doc.Text()
+                        }
+                        if field.Tag != nil {
+                           fieldInfo.Tag = field.Tag.Value
+                        }
+                        structInfo.Fields = append(structInfo.Fields, fieldInfo)
+                     }
+                  }
+               }
+               docInfo.Structs = append(docInfo.Structs, structInfo)
+               structMap[structInfo.Name] = structInfo
+            }
 
-			case *ast.FuncDecl:
-				if decl.Recv == nil || len(decl.Recv.List) == 0 {
-					return true
-				}
-				recvTypeNode := decl.Recv.List[0].Type
-				if star, ok := recvTypeNode.(*ast.StarExpr); ok { recvTypeNode = star.X }
-				ident, ok := recvTypeNode.(*ast.Ident)
-				if !ok { return true }
-				typeName := ident.Name
+         case *ast.FuncDecl:
+            if decl.Recv == nil || len(decl.Recv.List) == 0 {
+               return true
+            }
+            recvTypeNode := decl.Recv.List[0].Type
+            if star, ok := recvTypeNode.(*ast.StarExpr); ok {
+               recvTypeNode = star.X
+            }
+            ident, ok := recvTypeNode.(*ast.Ident)
+            if !ok {
+               return true
+            }
+            typeName := ident.Name
 
-				if structInfo, ok := structMap[typeName]; ok {
-					log.Printf("Found method '%s' for struct '%s'", decl.Name.Name, typeName)
-					
-					originalBody := decl.Body
-					originalDoc := decl.Doc
-					decl.Body = nil
-					decl.Doc = nil
-					signature := nodeToString(fset, decl)
-					decl.Body = originalBody
-					decl.Doc = originalDoc
+            if structInfo, ok := structMap[typeName]; ok {
+               log.Printf("Found method '%s' for struct '%s'", decl.Name.Name, typeName)
 
-					log.Printf(" - Generated Signature: '%s'", signature)
-					log.Printf(" - Receiver Type for linking: '%s'", typeName)
+               originalBody := decl.Body
+               originalDoc := decl.Doc
+               decl.Body = nil
+               decl.Doc = nil
+               signature := nodeToString(fset, decl)
+               decl.Body = originalBody
+               decl.Doc = originalDoc
 
-					structInfo.Methods = append(structInfo.Methods, MethodInfo{
-						Comment:   originalDoc.Text(),
-						Signature: signature,
-						RecvType:  typeName, // Storing the type name for the template
-					})
-				}
-			}
-			return true
-		})
-	}
-	return docInfo, nil
+               log.Printf(" - Generated Signature: '%s'", signature)
+               log.Printf(" - Receiver Type for linking: '%s'", typeName)
+
+               structInfo.Methods = append(structInfo.Methods, MethodInfo{
+                  Comment:   originalDoc.Text(),
+                  Signature: signature,
+                  RecvType:  typeName, // Storing the type name for the template
+               })
+            }
+         }
+         return true
+      })
+   }
+   return docInfo, nil
 }
 
 func nodeToString(fset *token.FileSet, node ast.Node) string {
-	var buf bytes.Buffer
-	printer.Fprint(&buf, fset, node)
-	return buf.String()
+   var buf bytes.Buffer
+   printer.Fprint(&buf, fset, node)
+   return buf.String()
 }
