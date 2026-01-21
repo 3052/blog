58d320004fc22acd8ca1767734c8092f034822c4
27 <27@riseup.net>
2026-01-21 16:25:37 -0600

    +      day_of: date(2026, 1, 29),

--- 2026/jog/jog.go
+++ 2026/jog/jog.go
@@ -9,9 +9,14 @@ import (
 
 var events = []jog{
    {
+      days_before: 9,
+      day_of: date(2026, 1, 29),
+      ok: nil,
+   },
+   {
       days_before: 10,
       day_of: date(2026, 1, 20),
-      ok: nil,
+      ok: some(true),
    },
    {
       days_before: 11,
